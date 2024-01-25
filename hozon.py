7bc5da0d46de66b135dbbd1176fa59aaccac4a2b
afuku08 <fukkuseiya@gmail.com>
2024-01-25 16:12:30 +0900

    モデルの保存

--- hozon.py
+++ hozon.py
@@ -8,9 +8,9 @@ ret, img = capture.read()
 #img = img[180:300, 223:403] #'ゴー'の位置
 #img = img[66:116, 105:208] #winの場合
 #img = img[91:170, 109:209] #loseの場合
-img = img[91:170, 433:533]
+#img = img[91:170, 433:533]
 
 #cv2.imshow("banmen", img)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
-cv2.imwrite("lose_d_e.png", img)
+cv2.imwrite("banmen4.png", img)
